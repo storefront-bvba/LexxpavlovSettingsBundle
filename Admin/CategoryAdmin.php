c07aa3f26d3260ab2524a25dae72050a6592ecd6
Wouter Samaey <wouter.samaey@storefront.be>
2019-04-10 08:31:41 +0200

    Fully functional

--- Admin/CategoryAdmin.php
+++ Admin/CategoryAdmin.php
@@ -10,73 +10,55 @@ use Sonata\AdminBundle\Show\ShowMapper;
 
 use App\Application\Lexxpavlov\SettingsBundle\Entity\Category;
 
-class CategoryAdmin extends AbstractAdmin
-{
-    public function configureListFields(ListMapper $listMapper)
-    {
-        $listMapper
-            ->addIdentifier('name')
-            ->add('comment')
-        ;
+class CategoryAdmin extends AbstractAdmin {
+    private $settingsService;
+
+    public function __construct($code, $class, $baseControllerName, \App\Application\Lexxpavlov\SettingsBundle\Service\Settings $settingsService) {
+        $this->settingsService = $settingsService;
+        parent::__construct($code, $class, $baseControllerName);
     }
 
-    public function configureFormFields(FormMapper $formMapper)
-    {
-        $formMapper
-            ->add('name')
-            ->add('comment')
-        ;
+    public function configureListFields(ListMapper $listMapper) {
+        $listMapper->addIdentifier('name')->add('comment');
     }
 
-    public function configureDatagridFilters(DatagridMapper $datagridMapper)
-    {
-        $datagridMapper
-            ->add('name')
-        ;
+    public function configureFormFields(FormMapper $formMapper) {
+        $formMapper->add('name')->add('comment');
     }
 
-    public function configureShowFields(ShowMapper $showMapper)
-    {
-        $showMapper
-            ->add('name')
-            ->add('comment')
-        ;
+    public function configureDatagridFilters(DatagridMapper $datagridMapper) {
+        $datagridMapper->add('name');
+    }
+
+    public function configureShowFields(ShowMapper $showMapper) {
+        $showMapper->add('name')->add('comment');
     }
 
     /**
      * @param Category $object
      */
-    public function postPersist($object)
-    {
+    public function postPersist($object) {
         $this->clearCache($object);
     }
 
     /**
      * @param Category $object
      */
-    public function postUpdate($object)
-    {
+    public function postUpdate($object) {
         $this->clearCache($object);
     }
 
     /**
      * @param Category $object
      */
-    public function preRemove($object)
-    {
+    public function preRemove($object) {
         $this->clearCache($object);
     }
 
     /**
      * @param Category $object
      */
-    private function clearCache(Category $object)
-    {
-
-        // TODO fix this. Causes error: The "lexxpavlov_settings.settings" service or alias has been removed or inlined when the container was compiled. You should either make it public, or stop using the container directly and use dependency injection instead.
-
-        /** @var \Lexxpavlov\SettingsBundle\Service\Settings $settings */
-//        $settings = $this->getConfigurationPool()->getContainer()->get('lexxpavlov_settings.settings');
-//        $settings->clearGroupCache($object->getName());
+    private function clearCache(Category $object) {
+        $this->settingsService->clearGroupCache($object->getName());
     }
 }
--- Admin/SettingsAdmin.php
+++ Admin/SettingsAdmin.php
@@ -15,126 +15,96 @@ use App\Application\Lexxpavlov\SettingsBundle\Entity\Category;
 use App\Application\Lexxpavlov\SettingsBundle\Form\Type\SettingValueType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
-class SettingsAdmin extends AbstractAdmin
-{
-    public function configureListFields(ListMapper $listMapper)
-    {
-        $useCategoryComment = $this->getConfigurationPool()->getContainer()
-            ->getParameter('lexxpavlov_settings.use_category_comment');
+class SettingsAdmin extends AbstractAdmin {
+    /**
+     * @var \Lexxpavlov\SettingsBundle\Service\Settings
+     */
+    private $settingsService;
 
-        $listMapper
-            ->addIdentifier('name')
-            ->add('category', null, array(
-                'associated_property' => function(Category $cat) use ($useCategoryComment) {
+    public function __construct($code, $class, $baseControllerName, \App\Application\Lexxpavlov\SettingsBundle\Service\Settings $settingsService) {
+        parent::__construct($code, $class, $baseControllerName);
+        $this->settingsService = $settingsService;
+    }
+
+    public function configureListFields(ListMapper $listMapper) {
+        $useCategoryComment = $this->getConfigurationPool()->getContainer()->getParameter('lexxpavlov_settings.use_category_comment');
+
+        $listMapper->addIdentifier('name')->add('category', null, array(
+                'associated_property' => function (Category $cat) use ($useCategoryComment) {
                     return $useCategoryComment && $cat->getComment() ? $cat->getComment() : $cat->getName();
                 },
                 'sortable' => true,
                 'sort_field_mapping' => array('fieldName' => 'name'),
                 'sort_parent_association_mappings' => array(array('fieldName' => 'category'))
-            ))
-            ->add('type', ChoiceType::class, array('choices' => SettingsType::getReadableValues(), 'catalogue' => 'messages'))
-            ->add('value', null, array('template' => 'LexxpavlovSettingsBundle:Admin:list_value.html.twig'))
-            ->add('comment')
-        ;
+            ))->add('type', ChoiceType::class, array(
+                'choices' => SettingsType::getReadableValues(),
+                'catalogue' => 'messages'
+            ))->add('value', null, array('template' => 'LexxpavlovSettingsBundle:Admin:list_value.html.twig'))->add('comment');
     }
 
-    public function configureFormFields(FormMapper $formMapper)
-    {
-        $valueType = $this->isNewForm()
-            ? SettingValueType::class
-            : 'setting_value';
-        $formMapper
-            ->add('name')
-            //->add('category', ModelAutocompleteType::class, ['property' => 'name'])
+    public function configureFormFields(FormMapper $formMapper) {
+        $valueType = $this->isNewForm() ? SettingValueType::class : 'setting_value';
+        $formMapper->add('name')//->add('category', ModelAutocompleteType::class, ['property' => 'name'])
             ->add('type', ChoiceType::class, array(
                 'choices' => SettingsType::getChoices(),
-                'attr' => array('data-sonata-select2'=>'false'),
-            ))
-            ->add('value', $valueType)
-            ->add('comment')
-        ;
+                'attr' => array('data-sonata-select2' => 'false'),
+            ))->add('value', $valueType)->add('comment');
     }
 
-    public function configureDatagridFilters(DatagridMapper $datagridMapper)
-    {
-        $useCategoryComment = $this->getConfigurationPool()->getContainer()
-            ->getParameter('lexxpavlov_settings.use_category_comment');
+    public function configureDatagridFilters(DatagridMapper $datagridMapper) {
+        $useCategoryComment = $this->getConfigurationPool()->getContainer()->getParameter('lexxpavlov_settings.use_category_comment');
 
-        $categoryOptions = $this->isNewForm()
-            ? array(
-                'choice_label' => function (Category $cat) use ($useCategoryComment) {
-                    return $useCategoryComment && $cat->getComment() ? $cat->getComment() : $cat->getName();
-                },
-            ) : array();
-        $datagridMapper
-            ->add('category', null, array(), null, $categoryOptions)
-            ->add('name')
-            ->add('type', null, array(), ChoiceType::class, array('choices' => SettingsType::getChoices()))
-        ;
+        $categoryOptions = $this->isNewForm() ? array(
+            'choice_label' => function (Category $cat) use ($useCategoryComment) {
+                return $useCategoryComment && $cat->getComment() ? $cat->getComment() : $cat->getName();
+            },
+        ) : array();
+        $datagridMapper->add('category', null, array(), null, $categoryOptions)->add('name')->add('type', null, array(), ChoiceType::class, array('choices' => SettingsType::getChoices()));
     }
 
-    public function configureShowFields(ShowMapper $showMapper)
-    {
-        $showMapper
-            ->add('name')
-            ->add('type')
-            ->add('value')
-            ->add('comment')
-        ;
+    public function configureShowFields(ShowMapper $showMapper) {
+        $showMapper->add('name')->add('type')->add('value')->add('comment');
     }
 
     /**
      * @param Settings $object
      */
-    public function postPersist($object)
-    {
+    public function postPersist($object) {
         $this->clearCache($object);
     }
 
     /**
      * @param Settings $object
      */
-    public function postUpdate($object)
-    {
+    public function postUpdate($object) {
         $this->clearCache($object);
     }
 
     /**
      * @param Settings $object
      */
-    public function preRemove($object)
-    {
+    public function preRemove($object) {
         $this->clearCache($object);
     }
 
-    public function getFormTheme()
-    {
-        return array_merge(
-            parent::getFormTheme(),
-            array('LexxpavlovSettingsBundle:Form:setting_value_edit.html.twig')
-        );
+    public function getFormTheme() {
+        return array_merge(parent::getFormTheme(), array('LexxpavlovSettingsBundle:Form:setting_value_edit.html.twig'));
     }
 
     /**
      * @param Settings $object
      */
-    private function clearCache(Settings $object)
-    {
-        // TODO This doesnt work, just like with the categories
-
-//        /** @var \Lexxpavlov\SettingsBundle\Service\Settings $settings */
-//        $settings = $this->getConfigurationPool()->getContainer()->get('lexxpavlov_settings.settings');
-//        $settings->clearCache($object->getName());
-//        if ($object->getCategory()) {
-//            $settings->clearGroupCache($object->getCategory()->getName());
-//        }
+    private function clearCache(Settings $object) {
+        $this->settingsService->clearCache($object->getName());
+        if ($object->getCategory()) {
+            $this->settingsService->clearGroupCache($object->getCategory()->getName());
+        }
     }
 
     /**
      * @return bool
      */
-    protected function isNewForm()
-    {
+    protected function isNewForm() {
         return method_exists('Symfony\Component\Form\AbstractType', 'getBlockPrefix');
     }
 }
